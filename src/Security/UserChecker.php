735bf7c1d00bd581f3b5e5d6dd366aa51d14b8ec
Maxim Sankouski <sankouski.m@gmail.com>
2022-01-23 14:32:15 +0300

    Add UserChecker (preAuth)

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Security;
+
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if ($user->getStatus() == 'Blocked') {
+            throw  new DisabledException('You are blocked.');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if ($user->getStatus() == 'Blocked') {
+            throw  new DisabledException('You are blocked.');
+        }
+    }
+
+}
